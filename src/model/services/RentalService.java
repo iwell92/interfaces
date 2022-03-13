d2e48f88da05c99d547d87cf51c8e35eee66c1d9
iwell92 <iwell92@live.com>
2022-03-12 21:01:02 -0300

    Trabalhando com interface.

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,37 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private TaxService taxServices;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxServices) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxServices = taxServices;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime(); /*No Java o tempo está em milissegundos*/
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60; /*t2 - t1 diferença em milissegundos*/ /*/1000 para converter em segundos*/ /*/60 para minutos*/ /*/60 transforma em horas*/
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour; /*Math.ceil arredonda as horas para cima*/
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxServices.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
